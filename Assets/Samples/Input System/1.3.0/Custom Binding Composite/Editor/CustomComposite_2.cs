cfa7c2866bcf99fc81dbc821c781f80b905266dc
Igor Ponomarev <felislord@gmail.com>
2023-07-14 12:11:55 +0300

    Add input sample

---
+++ Assets/Samples/Input System/1.3.0/Custom Binding Composite/Editor/CustomComposite_2.cs
@@ -0,0 +1,79 @@
+// Use InputBindingComposite<TValue> as a base class for a composite that returns
+// values of type TValue.
+// NOTE: It is possible to define a composite that returns different kinds of values
+//       but doing so requires deriving directly from InputBindingComposite.
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.Utilities;
+
+#if UNITY_EDITOR
+[InitializeOnLoad] // Automatically register in editor.
+#endif
+// Determine how GetBindingDisplayString() formats the composite by applying
+// the  DisplayStringFormat attribute.
+[DisplayStringFormat("{firstPart}+{secondPart}")]
+public class CustomComposite_2 : InputBindingComposite<float>
+{
+    // Each part binding is represented as a field of type int and annotated with
+    // InputControlAttribute. Setting "layout" restricts the controls that
+    // are made available for picking in the UI.
+    //
+    // On creation, the int value is set to an integer identifier for the binding
+    // part. This identifier can read values from InputBindingCompositeContext.
+    // See ReadValue() below.
+    [InputControl(layout = "Button")]
+    public int firstPart;
+
+    [InputControl(layout = "Button")]
+    public int secondPart;
+
+    // Any public field that is not annotated with InputControlAttribute is considered
+    // a parameter of the composite. This can be set graphically in the UI and also
+    // in the data (e.g. "custom(floatParameter=2.0)").
+    public float floatParameter;
+    public bool boolParameter;
+
+    // This method computes the resulting input value of the composite based
+    // on the input from its part bindings.
+    public override float ReadValue(ref InputBindingCompositeContext context)
+    {
+        var firstPartValue = context.ReadValue<float>(firstPart);
+        var secondPartValue = context.ReadValue<float>(secondPart);
+
+        var z = (firstPartValue + secondPartValue) / 2f;
+        return z;
+        //... do some processing and return value
+    }
+    
+    /*
+    // This method computes the current actuation of the binding as a whole.
+    public override float EvaluateMagnitude(ref InputBindingCompositeContext context)
+    {
+        // Compute normalized [0..1] magnitude value for current actuation level.
+        
+        return 2f;
+    }
+    */
+
+    static CustomComposite_2()
+    {
+        // Can give custom name or use default (type name with "Composite" clipped off).
+        // Same composite can be registered multiple times with different names to introduce
+        // aliases.
+        //
+        // NOTE: Registering from the static constructor using InitializeOnLoad and
+        //       RuntimeInitializeOnLoadMethod is only one way. You can register the
+        //       composite from wherever it works best for you. Note, however, that
+        //       the registration has to take place before the composite is first used
+        //       in a binding. Also, for the composite to show in the editor, it has
+        //       to be registered from code that runs in edit mode.
+        //InputSystem.RegisterBindingComposite<CustomComposite_2>();
+        //InputSystem.RegisterInteraction<MyWiggleInteraction>();
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Init() {} // Trigger static constructor.
+}
