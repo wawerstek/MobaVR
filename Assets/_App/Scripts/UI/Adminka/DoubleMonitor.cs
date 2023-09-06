acc85da055059e6773412473b6b234882158232c
YOGA\qweqw <rusli.kru2@gmial.com>
2023-09-06 16:44:57 +0300

    Добавлены камеры, изменены кнопки на спрайты, добавлена информация о карта, при наведении, добавлена возможность вывода на отдельынй экран камер.

---
+++ Assets/_App/Scripts/UI/Adminka/DoubleMonitor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleMonitor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+              
+        /*for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
