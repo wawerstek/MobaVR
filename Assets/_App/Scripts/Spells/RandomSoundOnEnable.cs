5850bc1c645304abfd5a39ef595060b405f40ed1
YOGA\qweqw <rusli.kru2@gmial.com>
2023-09-13 18:20:48 +0300

    Добавлен звук летящих шаров и прочей атаки

---
+++ Assets/_App/Scripts/Spells/RandomSoundOnEnable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomSoundOnEnable : MonoBehaviour
+{
+    public AudioClip[] sounds;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        PlayRandomSound();
+    }
+
+    private void PlayRandomSound()
+    {
+        if (sounds.Length == 0) return; // проверка на наличие звуков в массиве
+
+        int randomIndex = Random.Range(0, sounds.Length);
+        audioSource.clip = sounds[randomIndex];
+        audioSource.Play();
+    }
+}
